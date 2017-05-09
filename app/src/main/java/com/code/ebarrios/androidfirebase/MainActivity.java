ac8dc9c666291060b5f7757112b5ce5a46b59338
Eduardo Barrios <guayoswing@gmail.com>
2017-05-09 16:59:33 -0600

    Autenticación en firebase

--- app/src/main/java/com/code/ebarrios/androidfirebase/MainActivity.java
+++ app/src/main/java/com/code/ebarrios/androidfirebase/MainActivity.java
@@ -1,25 +1,56 @@
 package com.code.ebarrios.androidfirebase;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
-import com.code.ebarrios.androidfirebase.Objetos.Carro;
-import com.code.ebarrios.androidfirebase.Objetos.FirebaseReferencias;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    EditText txtCorreo;
+    EditText txtPassword;
+    Button btnLogin;
+    Button btnRegistro;
 
-    Button btnCarro;
+    FirebaseAuth.AuthStateListener mAuthStateListener;
+    //Button btnCarro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnCarro = (Button)findViewById(R.id.btnCarro);
+        txtCorreo = (EditText)findViewById(R.id.email);
+        txtPassword = (EditText)findViewById(R.id.password);
+        btnLogin = (Button)findViewById(R.id.btnLogin);
+        btnRegistro = (Button)findViewById(R.id.btnRegistro);
+
+        btnRegistro.setOnClickListener(this);
+        btnLogin.setOnClickListener(this);
+
+        mAuthStateListener = new FirebaseAuth.AuthStateListener(){
+
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser(); //FirebaseAuth.getInstance().getCurrentUser();
+
+                if(user != null)
+                {
+                    Log.i("SESION","Sesión iniciada con email "+user.getEmail());
+                }
+                else
+                {
+                    Log.i("SESION","Sesión cerrada");
+                }
+            }
+        };
+
+        /*btnCarro = (Button)findViewById(R.id.btnCarro);
 
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         final DatabaseReference TutorialReferencia = database.getReference(FirebaseReferencias.TUTORIAL_REFERENCIA);
@@ -51,5 +82,40 @@ public class MainActivity extends AppCompatActivity {
         };
         myRef.addValueEventListener(valueEventListener);
         */
+    }
+
+    private void Registrar(String email, String pass)
+    {
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email,pass);
+    }
+
+    private void IniciarSesion(String email, String pass)
+    {
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email,pass);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId())
+        {
+            case R.id.btnLogin:
+                String emailLogin = txtCorreo.getText().toString();
+                String passLogin = txtPassword.getText().toString();
+                IniciarSesion(emailLogin,passLogin);
+                break;
+
+            case R.id.btnRegistro:
+                String email = txtCorreo.getText().toString();
+                String pass = txtPassword.getText().toString();
+                Registrar(email,pass);
+                break;
+        }
+    }
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+        Fire
     }
 }
